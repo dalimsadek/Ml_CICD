2129e6f090b12c701ff2cc60be795888e0a2872e
dalimsadek <115195504+dalimsadek@users.noreply.github.com>
2025-07-02 23:09:40 +0200

    Update model.py

--- model.py
+++ model.py
@@ -30,17 +30,21 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
 
 
 def mae(y_test, y_pred):
-  """
-  Calculuates mean absolute error between y_test and y_preds.
-  """
-  return tf.metrics.mean_absolute_error(y_test, y_pred)
-  
+    """
+    Calculates mean absolute error between y_test and y_pred.
+    """
+    metric = tf.keras.metrics.MeanAbsoluteError()
+    metric.update_state(y_test, y_pred)
+    return metric.result().numpy()
 
 def mse(y_test, y_pred):
-  """
-  Calculates mean squared error between y_test and y_preds.
-  """
-  return tf.metrics.mean_squared_error(y_test, y_pred)
+    """
+    Calculates mean squared error between y_test and y_pred.
+    """
+    metric = tf.keras.metrics.MeanSquaredError()
+    metric.update_state(y_test, y_pred)
+    return metric.result().numpy()
+
 
 
 # Check Tensorflow version
